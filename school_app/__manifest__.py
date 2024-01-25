585a78bcc0e2ff1ee2e90340276a8daf6b60c92e
prince <prince.vekariya@bizzappdev.com>
2024-01-25 20:19:54 +0530

    adding in school app

---
+++ school_app/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "school_app",
+    "summary": """Basic School App""",
+    "description": """
+    """,
+    "author": "bizzappdev",
+    "website": "http://www.bizzappdev.com",
+    "category": "Uncategorized",
+    "version": "15.0.1.0.0",
+    "depends": ["base", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/teacher_menu_views.xml",
+        "views/teacher_views.xml",
+        "views/course_view.xml",
+    ],
+    "application": True,
+    "license": "LGPL-3",
+}
